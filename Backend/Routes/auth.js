e6fca0ce6b2692bddc3912389399b19d7716a580
Vanessa Onyinye Ezeh <vanessaezeh@meta.com>
2025-06-25 10:39:46 -0700

    ## Implementation - Removed all the console log and learnt from my peers to use throw error instead of console error  ##  Testplan - No Testing, Improving the quality of code

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -2,7 +2,7 @@ import express from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-import {PrismaClient} from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 dotenv.config();
 const router = express.Router();
@@ -23,7 +23,6 @@ router.post("/signup", async (req, res) => {
     }
 
     const existingUser = await prisma.user.findUnique({ where: { email } });
-    console.log(existingUser);
     if (existingUser) {
       return res.status(400).json({ message: "Email already in use" });
     }
@@ -37,7 +36,6 @@ router.post("/signup", async (req, res) => {
         password: hashedPassword,
       },
     });
-    console.log("new user created", newUser);
     const token = jwt.sign(
       { id: newUser.id, email: newUser.email },
       process.env.JWT_SECRET,
@@ -53,7 +51,7 @@ router.post("/signup", async (req, res) => {
       },
     });
   } catch (error) {
-    console.log("Signup error:", error);
+    throw new Error("Signup error:", error);
     res.status(500).json({ message: "Something went wrong during signup" });
   }
 });
@@ -65,14 +63,11 @@ router.post("/login", async (req, res) => {
   }
   try {
     const user = await prisma.user.findUnique({ where: { email } });
-
-
     if (!user) {
       return res
         .status(400)
         .json({ message: "Email or password is incorrect" });
     }
-
 
     const isValidPassword = await bcrypt.compare(password, user.password);
     if (!isValidPassword) {
@@ -80,7 +75,6 @@ router.post("/login", async (req, res) => {
         .status(400)
         .json({ message: "Email or password is incorrect" });
     }
-
 
     const token = jwt.sign(
       { id: user.id, email: user.email },
@@ -97,9 +91,9 @@ router.post("/login", async (req, res) => {
       },
     });
   } catch (error) {
-    console.log(error);
+    throw new Error (error);
     res.status(500).json({ message: "Something went wrong during login" });
   }
- });
+});
 
 export default router;
--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import authRouter from "./Routes/auth.js";
 
-
 const app = express();
 const PORT = 3000;
 
@@ -16,5 +15,4 @@ app.use("/auth",authRouter);
 
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
