00b26bf611454896c65fefd9609c2b5f3cde6db9
Vanessa Onyinye Ezeh <vanessaezeh@meta.com>
2025-06-24 23:24:18 -0700

    -Implementations: Implemented 'auth/login' using POST in 'auth.js' route for user signup with JWT ## TestPlan

--- Backend/Routes/auth.js
+++ Backend/Routes/auth.js
@@ -58,4 +58,48 @@ router.post("/signup", async (req, res) => {
   }
 });
 
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email and Password are required" });
+  }
+  try {
+    const user = await prisma.user.findUnique({ where: { email } });
+
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Email or password is incorrect" });
+    }
+
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return res
+        .status(400)
+        .json({ message: "Email or password is incorrect" });
+    }
+
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Something went wrong during login" });
+  }
+ });
+
 export default router;
