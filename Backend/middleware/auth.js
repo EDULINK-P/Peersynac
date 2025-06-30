c85385d4a00d68f220f6be645536c7afb7da1f75
Vanessa Onyinye Ezeh <vanessaezeh@meta.com>
2025-06-30 09:31:32 -0700

    User-Authentication-Implementation Implemented user authentication with session management using Express.js, Prisma, and Redis. Added validation for school email addresses by fetching from an external API. Created endpoints for signup, login, logout, and retrieving user details. Integrated password hashing and verification using bcrypt. Configured session management using express-session and Redis. ## Resources ## Test Plan

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,24 +1,9 @@
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-function verifyToken(req, res, next) {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer")) {
-    return res.status(401).json({ message: "No token provided" });
+const verifySession = (req, res, next) => {
+  if(!req.session.userId) {
+    res.status(401).send('You must be logged in to view this page.');
+    return
   }
+  next();
+  };
 
-  const token = authHeader.split(" ")[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    req.user = decoded;
-    next();
-  } catch (err) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-}
-
-export { verifyToken };
+export default verifySession;
